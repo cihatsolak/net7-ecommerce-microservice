5f47aeecabd9e1c1e2c52d2edd2064f33d469d50
Cihat Solak <cihatsolak@hotmail.com>
2022-02-12 21:54:14 +0300

    Configuration changes and api resources defined

--- identityserver/CourseSales.IdentityServer/Config.cs
+++ identityserver/CourseSales.IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -9,50 +10,58 @@ namespace CourseSales.IdentityServer
 {
     public static class Config
     {
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+        {
+            new ApiResource("resource_catalog")
+            {
+                Scopes =
+                {
+                    "catalog_fullpermission"
+                }
+            },
+            new ApiResource("resource_photo_stock")
+            {
+                Scopes =
+                {
+                    "photo_stock_fullpermission"
+                }
+            },
+            new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
+        };
+
         public static IEnumerable<IdentityResource> IdentityResources =>
                    new IdentityResource[]
                    {
-                new IdentityResources.OpenId(),
-                new IdentityResources.Profile(),
+
                    };
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
             {
-                new ApiScope("scope1"),
-                new ApiScope("scope2"),
+              new ApiScope("catalog_fullpermission","Full access to Catalog Api"),
+              new ApiScope("photo_stock_fullpermission","Full access to Photo Stock Api"),
+              new ApiScope(IdentityServerConstants.LocalApi.ScopeName),
             };
 
         public static IEnumerable<Client> Clients =>
             new Client[]
             {
-                // m2m client credentials flow client
-                new Client
+                new Client()
                 {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
-
+                    ClientName ="Asp.Net Core MVC",
+                    ClientId = "WebMvcClient",
+                    ClientSecrets =
+                    {
+                        new Secret("secret.".Sha256())
+                    },
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                    AllowedScopes = { "scope1" }
-                },
-
-                // interactive client using code flow + pkce
-                new Client
-                {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
-                    AllowedGrantTypes = GrantTypes.Code,
-
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
-                },
+                    AllowedScopes =
+                    {
+                        "catalog_fullpermission",
+                        "photo_stock_fullpermission",
+                        IdentityServerConstants.LocalApi.ScopeName
+                    }  
+                }
             };
     }
 }
--- identityserver/CourseSales.IdentityServer/Startup.cs
+++ identityserver/CourseSales.IdentityServer/Startup.cs
@@ -48,6 +48,7 @@ namespace CourseSales.IdentityServer
                 options.EmitStaticAudienceClaim = true;
             })
                 .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddInMemoryApiResources(Config.ApiResources)
                 .AddInMemoryApiScopes(Config.ApiScopes)
                 .AddInMemoryClients(Config.Clients)
                 .AddAspNetIdentity<ApplicationUser>();
