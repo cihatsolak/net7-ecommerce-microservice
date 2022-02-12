67d5b16fb0fa90d4de54a83935525d18be335145
Cihat Solak <cihatsolak@hotmail.com>
2022-02-12 22:36:57 +0300

    Added jwt token authentication for catalog service

--- services/catalog/CourseSales.Services.Catalog/Program.cs
+++ services/catalog/CourseSales.Services.Catalog/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(new AuthorizeFilter());
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,7 +15,17 @@ builder.Services.AddSingleton<IDatabaseSetting>(provider => provider.GetRequired
 builder.Services.AddScoped<IMongoContext, MongoContext>();
 builder.Services.AddScoped<ICategoryService, CategoryManager>();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServerUrl"];
+        options.Audience = "resource_catalog";
+        options.RequireHttpsMetadata = false;
+    });
+
 var app = builder.Build();
+
+app.UseAuthentication();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
