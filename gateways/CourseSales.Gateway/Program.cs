46e529728c666f3b68a10748e466d3971577798d
Cihat Solak <cihatsolak@hotmail.com>
2022-02-24 01:34:51 +0300

    Routes defined for ocelot

--- gateways/CourseSales.Gateway/Program.cs
+++ gateways/CourseSales.Gateway/Program.cs
@@ -1,9 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.ConfigureAppConfiguration((hostingContext, configure) =>
+{
+    configure
+    .AddJsonFile($"Configuration.{hostingContext.HostingEnvironment.EnvironmentName}.json")
+    .AddEnvironmentVariables();
+});
+
 builder.Services.AddOcelot();
 
 var app = builder.Build();
-
+app.UseHttpsRedirection();
 await app.UseOcelot();
 
 app.Run();
