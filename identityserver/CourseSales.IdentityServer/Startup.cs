6eec84bc7109714991f26d6dccb97e3c8e723d64
Cihat Solak <cihatsolak@hotmail.com>
2022-03-06 15:36:11 +0300

    token exchange extension grant validator add identity server  for token exchange

--- identityserver/CourseSales.IdentityServer/Startup.cs
+++ identityserver/CourseSales.IdentityServer/Startup.cs
@@ -58,6 +58,7 @@ namespace CourseSales.IdentityServer
                 .AddAspNetIdentity<ApplicationUser>();
 
             builder.AddResourceOwnerValidator<IdentityResourceOwnerPasswordValidator>();
+            builder.AddExtensionGrantValidator<TokenExchangeExtensionGrantValidator>();
 
             // not recommended for production - you need to store your key material somewhere secure
             builder.AddDeveloperSigningCredential();
